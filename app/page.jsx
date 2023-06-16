62a0d329e1d7c31fd3bcdee5ca274be7fdf4244a
TomaszM-dev <tomekmalocha@gmail.com>
2023-06-16 14:53:16 +0200

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" h-20">
+      <h3 className="opacity-90">hello esz</h3>
+    </div>
+  );
+};
+
+export default Home;
